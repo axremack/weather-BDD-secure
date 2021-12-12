b813369fda5ff04d243c5283d237b04b16f3f3a2
uke <ax.remack@gmail.com>
2021-12-12 15:20:03 +0100

    :sparkles: Adding regex to sanitize user entry

--- src/Main.java
+++ src/Main.java
@@ -14,21 +14,14 @@ public class Main {
         String url = "jdbc:sqlite:src/Database/weather.db";
         CityWeather weather = null;
 
-        if (args.length != 1) {
-            System.err.println("Usage : /usr/lib/jvm/java-11-openjdk-amd64/bin/java -Dfile.encoding=UTF-8 -classpath /mnt/c/Users/axrem/Documents/ZZ3/Java/TP6/out/production/TP6:/mnt/c/Users/axrem/Downloads/sqlite-jdbc-3.32.3.2.jar:/mnt/c/Users/axrem/Documents/ZZ3/Java/gson-2.8.8.jar Main city_name\n");
-            throw new IllegalArgumentException();
-        }
-
         Pattern p;
-        Matcher m;
-        // Regex searching for digit or special character except "-"
-        p = Pattern.compile("\\d");
-        m = p.matcher("Clermont");
-        if(m.find()) {
-            System.out.println("motif trouvé");
-        }
+        p = Pattern.compile("[^a-zA-Z!-]"); // Regex searching for digit or special character except "-"
 
         try {
+            if (args.length != 1 || p.matcher(args[0]).find()) {
+                throw new IllegalArgumentException();
+            }
+
             DBManager d = new DBManager(url);
             d.deleteOldData(); // Deleting data if too old
 
@@ -50,6 +43,8 @@ public class Main {
             }
 
             d.displayDBOrderedBy("city");
+        } catch (IllegalArgumentException e) {
+            System.err.println("Usage : /usr/lib/jvm/java-11-openjdk-amd64/bin/java -Dfile.encoding=UTF-8 -classpath /mnt/c/Users/axrem/Documents/ZZ3/Java/TP6/out/production/TP6:/mnt/c/Users/axrem/Downloads/sqlite-jdbc-3.32.3.2.jar:/mnt/c/Users/axrem/Documents/ZZ3/Java/gson-2.8.8.jar Main city_name\n");
         } catch (SQLException e){
             System.err.println("Error : invalid SQL request");
         } catch (ClassNotFoundException e) {
